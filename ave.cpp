1f5e10cddd85daca3f9c5f6affc724f74d9edc27
WatanabeMasato <kiksnave2210@gmail.com>
2021-06-12 10:47:45 +0900

    add file ave

---
+++ ave.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int x,y;
+
+	cout << "two integer of average\n";
+	cout << "x parameter "; cin >> x;
+	cout << "y parameter "; cin >> y;
+
+	double ave = (x+y)/2 ;
+	cout << "average of x and y" << ave << "\n";
+}
